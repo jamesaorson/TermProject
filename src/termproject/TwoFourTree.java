223e142416bb0d76ca40a48abb3fe7fd150083bd
Spencer Graffunder <spencergraffunder@cedarville.edu>
2016-12-05 16:02:01 -0500

    .

--- src/termproject/TwoFourTree.java
+++ src/termproject/TwoFourTree.java
@@ -80,95 +80,7 @@ public class TwoFourTree implements Dictionary {
         }
         
         return null;
-    }
-
-    public static void main(String[] args) {
-        Comparator myComp = new IntegerComparator();
-        TwoFourTree myTree = new TwoFourTree(myComp);
-
-        Integer myInt1 = new Integer(47);
-        myTree.insertElement(myInt1, myInt1);
-        Integer myInt2 = new Integer(83);
-        myTree.insertElement(myInt2, myInt2);
-        Integer myInt3 = new Integer(22);
-        myTree.insertElement(myInt3, myInt3);
-
-        Integer myInt4 = new Integer(16);
-        myTree.insertElement(myInt4, myInt4);
-
-        Integer myInt5 = new Integer(49);
-        myTree.insertElement(myInt5, myInt5);
-
-        Integer myInt6 = new Integer(100);
-        myTree.insertElement(myInt6, myInt6);
-
-        Integer myInt7 = new Integer(38);
-        myTree.insertElement(myInt7, myInt7);
-
-        Integer myInt8 = new Integer(3);
-        myTree.insertElement(myInt8, myInt8);
-
-        Integer myInt9 = new Integer(53);
-        myTree.insertElement(myInt9, myInt9);
-
-        Integer myInt10 = new Integer(66);
-        myTree.insertElement(myInt10, myInt10);
-
-        Integer myInt11 = new Integer(19);
-        myTree.insertElement(myInt11, myInt11);
-
-        Integer myInt12 = new Integer(23);
-        myTree.insertElement(myInt12, myInt12);
-
-        Integer myInt13 = new Integer(24);
-        myTree.insertElement(myInt13, myInt13);
-
-        Integer myInt14 = new Integer(88);
-        myTree.insertElement(myInt14, myInt14);
-
-        Integer myInt15 = new Integer(1);
-        myTree.insertElement(myInt15, myInt15);
-
-        Integer myInt16 = new Integer(97);
-        myTree.insertElement(myInt16, myInt16);
-
-        Integer myInt17 = new Integer(94);
-        myTree.insertElement(myInt17, myInt17);
-
-        Integer myInt18 = new Integer(35);
-        myTree.insertElement(myInt18, myInt18);
-
-        Integer myInt19 = new Integer(51);
-        myTree.insertElement(myInt19, myInt19);
-
-        myTree.printAllElements();
-        System.out.println("done");
-
-        myTree = new TwoFourTree(myComp);
-        final int TEST_SIZE = 10000;
-
-
-        for (int i = 0; i < TEST_SIZE; i++) {
-            myTree.insertElement(new Integer(i), new Integer(i));
-            //myTree.printAllElements();
-            //myTree.checkTree();
-        }
-        
-        System.out.println("removing");
-        
-        for (int i = 0; i < TEST_SIZE; i++) {
-            int out = (Integer) myTree.removeElement(new Integer(i));
-        
-            if (out != i) {
-                throw new TwoFourTreeException("main: wrong element removed");
-            }
-            if (i > (TEST_SIZE - 15)) {
-                myTree.printAllElements();
-            }
-        }
-        
-        System.out.println("done");
-    }
+	}
 
     public void printAllElements() {
         int indent = 0;
@@ -268,4 +180,93 @@ public class TwoFourTree implements Dictionary {
             checkTreeFromNode(start.getChild(childIndex));
         }
     }
+	
+	public static void main(String[] args) {
+        Comparator myComp = new IntegerComparator();
+        TwoFourTree myTree = new TwoFourTree(myComp);
+
+        Integer myInt1 = new Integer(47);
+        myTree.insertElement(myInt1, myInt1);
+        Integer myInt2 = new Integer(83);
+        myTree.insertElement(myInt2, myInt2);
+        Integer myInt3 = new Integer(22);
+        myTree.insertElement(myInt3, myInt3);
+
+        Integer myInt4 = new Integer(16);
+        myTree.insertElement(myInt4, myInt4);
+
+        Integer myInt5 = new Integer(49);
+        myTree.insertElement(myInt5, myInt5);
+
+        Integer myInt6 = new Integer(100);
+        myTree.insertElement(myInt6, myInt6);
+
+        Integer myInt7 = new Integer(38);
+        myTree.insertElement(myInt7, myInt7);
+
+        Integer myInt8 = new Integer(3);
+        myTree.insertElement(myInt8, myInt8);
+
+        Integer myInt9 = new Integer(53);
+        myTree.insertElement(myInt9, myInt9);
+
+        Integer myInt10 = new Integer(66);
+        myTree.insertElement(myInt10, myInt10);
+
+        Integer myInt11 = new Integer(19);
+        myTree.insertElement(myInt11, myInt11);
+
+        Integer myInt12 = new Integer(23);
+        myTree.insertElement(myInt12, myInt12);
+
+        Integer myInt13 = new Integer(24);
+        myTree.insertElement(myInt13, myInt13);
+
+        Integer myInt14 = new Integer(88);
+        myTree.insertElement(myInt14, myInt14);
+
+        Integer myInt15 = new Integer(1);
+        myTree.insertElement(myInt15, myInt15);
+
+        Integer myInt16 = new Integer(97);
+        myTree.insertElement(myInt16, myInt16);
+
+        Integer myInt17 = new Integer(94);
+        myTree.insertElement(myInt17, myInt17);
+
+        Integer myInt18 = new Integer(35);
+        myTree.insertElement(myInt18, myInt18);
+
+        Integer myInt19 = new Integer(51);
+        myTree.insertElement(myInt19, myInt19);
+
+        myTree.printAllElements();
+        System.out.println("done");
+
+        myTree = new TwoFourTree(myComp);
+        final int TEST_SIZE = 10000;
+
+
+        for (int i = 0; i < TEST_SIZE; i++) {
+            myTree.insertElement(new Integer(i), new Integer(i));
+            //myTree.printAllElements();
+            //myTree.checkTree();
+        }
+        
+        System.out.println("removing");
+        
+        for (int i = 0; i < TEST_SIZE; i++) {
+            int out = (Integer) myTree.removeElement(new Integer(i));
+        
+            if (out != i) {
+                throw new TwoFourTreeException("main: wrong element removed");
+            }
+            if (i > (TEST_SIZE - 15)) {
+                myTree.printAllElements();
+            }
+        }
+        
+        System.out.println("done");
+    }
+	
 }
